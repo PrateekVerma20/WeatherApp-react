7637f5a1aecc2a10c1c0334120b3ef7a53f4dd0e
PrateekVerma20 <prateekverma1998@gmail.com>
2020-04-27 00:44:52 +0530

    Updates

---
+++ precache-manifest.0b78f61d8963207bb771d4893aa94e99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abc4d896cd0c4e74c5ff01503d1aa2e",
+    "url": "/WeatherApp-react/index.html"
+  },
+  {
+    "revision": "49383993d7c97b46fb56",
+    "url": "/WeatherApp-react/static/css/main.0109d55e.chunk.css"
+  },
+  {
+    "revision": "a2a29b7117987b2d9e1e",
+    "url": "/WeatherApp-react/static/js/2.11df2935.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp-react/static/js/2.11df2935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49383993d7c97b46fb56",
+    "url": "/WeatherApp-react/static/js/main.4144f864.chunk.js"
+  },
+  {
+    "revision": "5dd004d1739697b49716",
+    "url": "/WeatherApp-react/static/js/runtime-main.658a7f4b.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/WeatherApp-react/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/WeatherApp-react/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
